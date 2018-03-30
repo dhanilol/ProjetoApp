b072e06d8ea7412ae897c4d4538950a76a76c1c6
DESKTOP-0JE9SRD\danilol <dhanilol@bitbucket.org>
2018-03-30 14:17:36 -0300

    user info in menu and some other stuff....

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
 import { LivroListPage } from '../pages/livro-list/livro-list';
+import { UserPage } from '../pages/user/user';
 
 @Component({
     templateUrl: 'app.html'
